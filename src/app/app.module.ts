4c111c0b510f55094ea33f8af25e9f96aee1fc0f
Dolan <dolan_miu@hotmail.com>
2017-09-10 00:00:25 +0100

    Add footer and fix linting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FooterModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
