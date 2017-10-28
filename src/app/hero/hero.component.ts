233f933fd34002e8dd820e346cf51f1cfadc311a
Dolan <dolan_miu@hotmail.com>
2017-10-28 01:45:58 +0100

    Add slide show photos to hero component

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+interface HeroImage {
+    name: string;
+    active?: boolean;
+}
+
 @Component({
     selector: 'app-hero',
     templateUrl: './hero.component.html',
     styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+    public images: HeroImage[];
 
-    constructor() { }
+    constructor() {
+        this.images = [{
+            name: 'Beef noodles',
+        }, {
+            name: 'Beef Sushi',
+        }, {
+            name: 'Chicken Sushi',
+        }, {
+            name: 'Chicken02',
+        }, {
+            name: 'Fried chicken01',
+        }, {
+            name: 'Range',
+        }, {
+            name: 'Rice',
+        }, {
+            name: 'Rice02',
+        }, {
+            name: 'Salmon Sushi',
+        }, {
+            name: 'Seafood pancake01',
+        }, {
+            name: 'Spicy pasta',
+        }, {
+            name: 'Spicy soup with rice',
+        }, {
+            name: 'Stone rice_01',
+        }, {
+            name: 'Veg Sushi',
+        }];
+
+        const index = Math.floor(Math.random() * this.images.length);
+
+        this.images[index].active = true;
+    }
 
     public ngOnInit(): void {
     }
