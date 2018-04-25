a73a34bc279de3a6e470f862138f2138bb738f71
Dolan <dolan_miu@hotmail.com>
2018-04-25 23:44:38 +0100

    Responsiveness

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -15,9 +15,19 @@ export class FooterComponent implements OnInit {
     public maxZoom = 17;
     public streetViewControl = false;
 
-    constructor() { }
+    constructor() {}
 
-    public ngOnInit(): void {
+    public ngOnInit(): void {}
+
+    public navToGoogleMaps(): void {
+        if (
+            navigator.platform.indexOf('iPhone') !== -1 ||
+            navigator.platform.indexOf('iPod') !== -1 ||
+            navigator.platform.indexOf('iPad') !== -1
+        ) {
+            window.open('maps://maps.google.com/maps?daddr=lat,long&amp;ll=');
+        } else {
+            window.open('http://maps.google.com/maps?daddr=lat,long&amp;ll=');
+        }
     }
-
 }
